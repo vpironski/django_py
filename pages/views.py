0ae57685e630409b7717762a81f095b9a2c77961
vpironski <vpironski@gmail.com>
2025-03-04 13:18:08 +0200

    Adding basic html files, for home, about and header page

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):
+    template_name = "pages/home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
